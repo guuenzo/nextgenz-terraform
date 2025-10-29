75062238d4e135eaf36d5cf04a6b48134ccd23b8
guuenzo <gustavoenzo463@gmail.com>
2025-10-29 10:35:39 -0300

    instalado os modulos de iam networking(vpc) e kubernetes(cluster, nodes e addons)

--- main.tf
+++ main.tf
@@ -1,1 +1,71 @@
-#CHAMADA DOS MODULES
+module "networking" {
+  source  = "app.terraform.io/tcc_senai/networking/aws"
+  version = "1.0.0"
+  # insert required variables here
+  vpc_name = "nextgenz"
+  vpc_cidr = "172.16.0.0/16"
+  public_a_cidr = "172.16.1.0/24"
+  public_b_cidr = "172.16.2.0/24"
+  private_a_cidr = "172.16.3.0/24"
+  private_b_cidr = "172.16.4.0/24"
+  az_a = "${var.aws_region}a"
+  az_b = "${var.aws_region}b"
+  environment = "prod"
+}
+
+module "iam" {
+  source  = "app.terraform.io/tcc_senai/iam/aws"
+  version = "1.0.0"
+  # insert required variables here
+  cluster_role_name = "ClusterRole"
+  node_role_name = "NodeRole"
+}
+
+module "eks" {
+  source  = "app.terraform.io/tcc_senai/eks/aws"
+  version = "1.0.0"
+  # insert required variables here
+  cluster_name = "nextgenz"
+  cluster_subnets_id = [ module.networking.public_sub_a_id, module.networking.public_sub_b_id, module.networking.private_sub_a_id, module.networking.private_sub_b_id ]
+  cluster_sg_id = module.networking.cluster_sg_id
+  k8s_cluster_role_arn = module.iam.cluster_role_arn
+  k8s_version = "1.33"
+  node_group_role_arn = module.iam.node_role_arn
+  node_group_subnets_id = [module.networking.private_sub_a_id, module.networking.private_sub_b_id]
+  environment = "prod"
+
+  depends_on = [ module.networking, module.iam ]
+}
+
+module "alb_controller" {
+  source  = "app.terraform.io/tcc_senai/alb_controller/helm"
+  version = "1.0.0"
+  # insert required variables here
+  cluster_name = module.eks.cluster_name
+  aws_region = var.aws_region
+  vpc_id = module.networking.vpc_id
+  eks_cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
+
+  depends_on = [ module.eks, module.iam ]
+}
+
+module "argocd" {
+  source  = "app.terraform.io/tcc_senai/argocd/helm"
+  version = "1.0.0"
+
+  depends_on = [ module.eks ]
+}
+
+module "keda" {
+  source  = "app.terraform.io/tcc_senai/keda/helm"
+  version = "1.0.0"
+
+  depends_on = [ module.eks ]
+}
+
+module "metrics_server" {
+  source  = "app.terraform.io/tcc_senai/metrics_server/helm"
+  version = "1.0.0"
+
+  depends_on = [ module.eks ]
+}
