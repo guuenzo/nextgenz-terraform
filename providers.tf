ecd734fa4a8b404439d2baf991d395140454a882
guuenzo <gustavoenzo463@gmail.com>
2025-10-30 07:58:10 -0300

    adicionado os providers helm e kubernetes

--- providers.tf
+++ providers.tf
@@ -1,4 +1,22 @@
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
